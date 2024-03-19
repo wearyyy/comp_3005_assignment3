3ce0da321340a3e84aff7d8dc062f75c0ba0f409
AdrianWong <67983460+wearyyy@users.noreply.github.com>
2024-03-18 22:26:13 -0400

    Delete School database creation.sql

--- School database creation.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE students(
-	student_id		SERIAL			PRIMARY KEY,
-	first_name		VARCHAR(255)	NOT NULL,
-	last_name		VARCHAR(255)	NOT NULL,
-	email			VARCHAR(255)	UNIQUE NOT NULL ,
-	enrollment_date	DATE
-)
